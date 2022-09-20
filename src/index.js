25586804f630ccc01935708255e1341432675154
Ihor <ihorrykun@gmail.com>
2022-09-20 11:53:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import './css/styles.css';
 import throttle from 'lodash.throttle';
+import { debounce } from 'lodash';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   input: document.querySelector('#search-box'),
   ul: document.querySelector('.country-list'),
   div: document.querySelector('.country-info'),
 };
-
-
 const DEBOUNCE_DELAY = 300;
+
+
+refs.input.addEventListener('input', debounce(as,DEBOUNCE_DELAY));
+
+
+function as() {
+
+}
