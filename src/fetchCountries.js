93f046aadee2386c4a261b7c409f6242eaf5e25e
Ihor <ihorrykun@gmail.com>
2022-09-22 18:28:40 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,12 @@
 function fetchCountries() {
-    return fetch(
-      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-    );
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+        .then(resp => {
+            if (!resp.ok) {
+                throw new Error(resp.status);
+            }
+            return resp.json();
+        });
 }
+
+
+export default { fetchCountries };
